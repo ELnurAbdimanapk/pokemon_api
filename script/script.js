7304e885b973badedad98a081bfb4015351ffbf1
ELnurAbdimanapk <elnurabdimanapkyzy@gmail.com>
2023-05-12 10:59:43 +0600

    added loading part

--- script/script.js
+++ script/script.js
@@ -1,20 +1,24 @@
- let pokemonList = []
- async function fetchPokemon () {
+let pokemonList = [];
+// Loading
+const container = document.getElementById("container");
+container.innerHTML = "";
+const pokemonWrapper = document.createElement("div");
+pokemonWrapper.classList.add("pokemon");
+const message = document.createElement("h1");
+message.textContent = "Loading..... ";
+pokemonWrapper.appendChild(message);
+container.appendChild(pokemonWrapper);
+
+async function fetchPokemon() {
   try {
-    const response =await fetch ('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
     const data = await response.json();
-     pokemonList = data.results;
-    displayPokemonData(pokemonList)
-
-  }catch(error){
+    pokemonList = data.results;
+    displayPokemonData(pokemonList);
+    console.log("loading");
+  } catch (error) {
     console.log(error);
-    const container = document.getElementById('container')
-    container.innerText= "Failed to fetch Pokemon"
+    const container = document.getElementById("container");
+    container.innerText = "Failed to fetch Pokemon";
   }
- }
-
- 
-
-
-
-
+}
